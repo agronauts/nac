453f9412f5dd50b3b2e53a6f9e3c7c031ad1ae76
Patrick Nicholls <patrickn@estaroffice.local>
2017-01-18 15:59:28 +1300

    Tile type checking, tile can be nought

--- board.py
+++ board.py
@@ -1,15 +1,29 @@
 class Tile(object):
+    accepted_tokens = ['x', 'o', '-']
 
     def __init__(self, typ):
+        token = typ.lower()
+        if token not in self.accepted_tokens:
+            raise Exception('Not a Tile type')
         self._typ = typ.lower()
-    
+
     def is_cross(self):
         return self._typ == 'x'
 
+    def is_nought(self):
+        return self._typ == 'o'
+
+    def is_empty(self):
+        return self._typ == '-'
+
 class Board:
-    board = [['-' for _ in range(3)] for _ in range(3)]
+    _board = [['-' for _ in range(3)] for _ in range(3)]
+
+    def __setitem__(self, index, item):
+        tile = Tile(item)
+        self._board[index] = tile
 
     def __getitem__(self, index):
-        return self.board[index]
+        return self._board[index]
 
 
--- tests.py
+++ tests.py
@@ -12,10 +12,25 @@ class BoardTest(unittest.TestCase):
 
 class TileTest(unittest.TestCase):
 
+    def test_is_nought(self):
+        tile = Tile('o')
+
+        self.assertTrue(tile.is_nought())
+
     def test_is_cross(self):
         tile = Tile('x')
 
         self.assertTrue(tile.is_cross())
+
+    def test_is_empty(self):
+        tile = Tile('-')
+
+        self.assertTrue(tile.is_empty())
+
+    def test_invalid_tile(self):
+        with self.assertRaises(Exception) as context:
+            Tile('invalid')
+        self.assertTrue('Not a Tile type', str(context.exception))
 
 def main():
     unittest.main()
